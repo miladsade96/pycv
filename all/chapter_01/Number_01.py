d19013d08c034d4d1b070afcb1d5c8b97ddc56a4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-25 22:02:10 +0430

    Creating a random image and convert it to a 2bit image

--- all/chapter_01/Number_01.py
+++ all/chapter_01/Number_01.py
@@ -3,3 +3,8 @@
 # import statements
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# Creating a random image and convert it to a 2bit image
+rnd_img = np.random.uniform(low=0, high=3, size=(5, 5))
+rnd_img = np.unit8(rnd_img)
